d1a7104346096a717cef822ce787770d4f584ef7
BjornWest <70112340+BjornWest@users.noreply.github.com>
2022-09-09 16:37:49 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,12 @@ input.onButtonPressed(Button.A, function () {
         if (UpDown % 2 == 0) {
             if (xPosition > 0) {
                 xPosition += -1
-                radio.sendNumber(1)
+                radio.sendValue("xPos", xPosition)
             }
         } else {
             if (yPosition > 0) {
                 yPosition += -1
-                radio.sendNumber(3)
+                radio.sendValue("yPos", yPosition)
             }
         }
     } else if (mode == 1) {
@@ -19,10 +19,16 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    let yTarget = 0
-    let xTarget = 0
     if (xPosition == xTarget && yPosition == yTarget) {
-        mode += 1
+        if (xPosition < 5 && yPosition < 5) {
+            radio.sendValue("interact", 1)
+        } else if (xPosition > 5 && yPosition < 5) {
+            radio.sendValue("interact", 2)
+        } else if (xPosition < 5 && yPosition > 5) {
+            radio.sendValue("interact", 3)
+        } else {
+            radio.sendValue("interact", 4)
+        }
     } else {
         UpDown += 1
     }
@@ -33,12 +39,12 @@ input.onButtonPressed(Button.B, function () {
         if (UpDown % 2 == 0) {
             if (xPosition < 4) {
                 xPosition += 1
-                radio.sendNumber(0)
+                radio.sendValue("xPos", yPosition)
             }
         } else {
             if (yPosition < 4) {
                 yPosition += 1
-                radio.sendNumber(2)
+                radio.sendValue("yPos", yPosition)
             }
         }
     } else if (mode == 1) {
@@ -47,7 +53,17 @@ input.onButtonPressed(Button.B, function () {
     	
     }
 })
+radio.onReceivedValue(function (name, value) {
+    if (name == "xTarget") {
+        xTarget = value
+    }
+    if (name == "yTarget") {
+        yTarget = value
+    }
+})
 let UpDown = 0
+let yTarget = 0
+let xTarget = 0
 let yPosition = 0
 let xPosition = 0
 let mode = 0
@@ -55,25 +71,11 @@ mode = 0
 radio.setGroup(1)
 xPosition = 0
 yPosition = 0
-let star1 = images.createImage(`
-    . . . . .
-    . # . # .
-    . . # . .
-    . # . # .
-    . . . . .
-    `)
-let star2 = images.createImage(`
-    . . . . .
-    . . # . .
-    . # # # .
-    . . # . .
-    . . . . .
-    `)
+xTarget = 0
+yTarget = 0
 loops.everyInterval(1000, function () {
     if (mode == 0) {
-        star1.showImage(0, 1000)
-        star2.showImage(0, 1000)
-        basic.clearScreen()
+    	
     }
 })
 basic.forever(function () {
